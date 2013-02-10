5f31fd167d6846a57fccc5664808c2f2c97e2e22
Niedermann Fabian <fabian.niedermann@thurweb.ch>
2013-02-10 17:14:19 +0100

    initial commit with Xcode project

---
+++ MathSolver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MathSolver
+//
+//  Created by Niedermann Fabian on 10.02.13.
+//  Copyright (c) 2013 Niedermann Fabian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MathSolver/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MathSolver
+//
+//  Created by Niedermann Fabian on 10.02.13.
+//  Copyright (c) 2013 Niedermann Fabian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
